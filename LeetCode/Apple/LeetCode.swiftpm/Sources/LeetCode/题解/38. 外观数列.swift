9df0f3f8478966a334b79000ca5f641a0d1d2c66
Loovemaker <bows-02.roper@icloud.com>
2022-07-20 22:26:14 +0800

    solved LeetCode 38. 外观数列

---
+++ LeetCode/Apple/LeetCode.swiftpm/Sources/LeetCode/题解/38. 外观数列.swift
@@ -0,0 +1,50 @@
+public extension Solution {
+    
+    /// <doc:38.----->
+    ///
+    /// - ⌛️：59%
+    /// - 🫙：50%
+    func countAndSay(_ n: Int) -> String {
+        precondition(n > 0)
+        let cache = ["", "1", "11", "21", "1211", "111221"]
+        
+        if n < cache.count {
+            return cache[n]
+        }
+        
+        var result = cache.last!
+        for _ in cache.count ... n {
+            var newResult = ""
+            var memory: Memory = (0, nil)
+            for char in result {
+                if let mchar = memory.char, char != mchar {
+                    newResult.append(Character(String(memory.count)))
+                    newResult.append(Character(String(mchar.hexDigitValue!)))
+                    memory = (0, nil)
+                }
+                memory.count += 1
+                memory.char = char
+                
+            }
+            
+            clearMemory(char: nil, memory: &memory, result: &newResult)
+            
+            result = newResult
+        }
+        
+        return result
+    }
+    
+    fileprivate typealias Memory = (count: Int, char: Character?)
+    
+    fileprivate func clearMemory(char: Character?,
+                                 memory: inout Memory,
+                                 result: inout String
+    ) {
+        if let mchar = memory.char, char != mchar {
+            result.append(Character(String(memory.count)))
+            result.append(Character(String(mchar.hexDigitValue!)))
+            memory = (0, nil)
+        }
+    }
+}
